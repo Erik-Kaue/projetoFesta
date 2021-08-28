b42b6998255062bcbbbd9e6f7f45f5cd4db50e0e
Erik-Kaue <dexplay122@hotmail.com>
2021-08-28 11:09:53 +0900

    primeira função

--- www/components/script.js
+++ www/components/script.js
@@ -1,2 +1,11 @@
 // This is a JavaScript file
+window.onload = function(){
+  const valor1 = document.querySelector("#valor1");
+  const btn = document.querySelector("#btn");
+
+  btn.addEventListener("click", function(){
+    let resRefri = (parseFloat(valor1.value) * 600) / 1000;
+    document.querySelector("#refri").value = resRefri + " Litros";
+});
+}
 
