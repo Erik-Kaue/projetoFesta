f770b1241760dca11f020d3a8d9dedf62c39c453
Erik-Kaue <dexplay122@hotmail.com>
2021-09-02 09:00:58 +0900

    Java script pronto

--- www/components/script.js
+++ www/components/script.js
@@ -6,6 +6,14 @@ window.onload = function(){
   btn.addEventListener("click", function(){
     let resRefri = (parseFloat(valor1.value) * 600) / 1000;
     document.querySelector("#refri").value = resRefri + " Litros";
+    let resAgua = (parseFloat(valor1.value) * 200) / 1000;
+    document.querySelector("#agua").value = resAgua + " Litros";
+    let resBolo = (parseFloat(valor1.value) * 100) / 1000;
+    document.querySelector("#bolo").value = resBolo + " Quilos";
+    let resDoce = parseFloat(valor1.value) * 3;
+    document.querySelector('#doce').value = resDoce + " Docês em geral";
+    let resSalgado = parseFloat(valor1.value) * 10;
+    document.querySelector("#salgado").value = resSalgado + " Salgadinhos";
 });
 }
 
